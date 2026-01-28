58f18254fdbdc4edcb09a7484f3cade034379fe2
gyaanbotics <gyaanbotics@gmail.com>
2026-01-28 06:07:57 +0530

    Update normalize-url.js

--- functions/normalize-url.js
+++ functions/normalize-url.js
@@ -1,9 +1,15 @@
 export function onRequest({ request }) {
   const url = new URL(request.url);
 
-  // Redirect /index.html to /
+  // Redirect index.html → /
   if (url.pathname.endsWith("/index.html")) {
-    url.pathname = url.pathname.replace("/index.html", "/");
+    url.pathname = "/";
+    return Response.redirect(url.toString(), 301);
+  }
+
+  // Strip query parameters
+  if (url.search) {
+    url.search = "";
     return Response.redirect(url.toString(), 301);
   }
 
